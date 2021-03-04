635a201e80fb601cba4afb7400d121a85a7ba523
alessiarocco98 <alessia.rocco@studio.unibo.it>
2021-03-04 18:54:42 +0100

    exercise 2 step 1 done

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,61 @@
+package lab01.tdd;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+    private final List<Integer> List;
+    private Integer currentIndexElement;
+
+    public CircularListImpl() {
+        List = new LinkedList<>();
+        currentIndexElement = null;
+    }
+
+    @Override
+    public void add(int element) {
+        List.add(element);
+    }
+
+    @Override
+    public int size() {
+        return List.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return List.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        if(currentIndexElement == null || currentIndexElement == List.size() - 1 ) {
+            currentIndexElement = 0;
+            return Optional.of(List.get(currentIndexElement));
+        } else {
+
+            return Optional.of(List.get(++currentIndexElement));
+        }
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        if(currentIndexElement == null || currentIndexElement == 0 ) {
+            currentIndexElement = List.size() - 1;
+            return Optional.of(List.get(currentIndexElement));
+        } else {
+            return Optional.of(List.get(--currentIndexElement));
+        }
+    }
+
+    @Override
+    public void reset() {
+        currentIndexElement = 0;
+    }
+
+    @Override
+    public Optional<Integer> next(SelectStrategy strategy) {
+        return Optional.empty();
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,13 +1,78 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
+
+    @BeforeEach
+    void beforeEach() {
+        circularList = new CircularListImpl();
+    }
+
+    @Test
+    void testAdd() {
+        circularList.add(1);
+        assertEquals(1, circularList.size());
+    }
+
+    @Test
+    void testEmpty(){
+        assertTrue(circularList.isEmpty());
+        circularList.add(1);
+        assertFalse(circularList.isEmpty());
+    }
+
+    @Test
+    void testSize() {
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(3, circularList.size());
+    }
+
+    @Test
+    void testNext(){
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(Optional.of(1), circularList.next());
+        assertEquals(Optional.of(2), circularList.next());
+        assertEquals(Optional.of(3), circularList.next());
+        assertEquals(Optional.of(1), circularList.next());
+    }
+
+    @Test
+    void testPrev(){
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(Optional.of(3), circularList.previous());
+        assertEquals(Optional.of(2), circularList.previous());
+        assertEquals(Optional.of(1), circularList.previous());
+        assertEquals(Optional.of(3), circularList.previous());
+    }
+    @Test
+    void testReset() {
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        circularList.next();
+        circularList.next();
+        circularList.reset();
+        assertEquals(Optional.of(2), circularList.next());
+    }
 
     @Disabled
     @Test public void testTodo(){
